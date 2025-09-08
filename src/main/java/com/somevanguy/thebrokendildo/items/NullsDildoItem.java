03dc7327f9436c5c9d7b19c43d4e0b1fc0392d75
SomVanTeam <shalimovandrey32@gmail.com>
2025-09-09 01:23:17 +0300

    nulls dildo texture

--- src/main/java/com/somevanguy/thebrokendildo/items/NullsDildoItem.java
+++ src/main/java/com/somevanguy/thebrokendildo/items/NullsDildoItem.java
@@ -10,12 +10,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 public class NullsDildoItem extends Item {
     public NullsDildoItem() {
-        super(new Properties().rarity(Rarity.EPIC).durability(7));
+        super(new Properties().rarity(Rarity.EPIC).durability(676767));
     }
 
     @Override
